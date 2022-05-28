4e24a410344798f6c704037d4fa7827a0717c7ae
binaryleo <leomouracss@fastmail.com>
2022-05-28 15:09:38 -0300

    chore: button state - show project list

--- app/src/main/java/com/binaryleo/jetpack_business_card/MainActivity.kt
+++ app/src/main/java/com/binaryleo/jetpack_business_card/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.binaryleo.jetpack_business_card
 
+import android.content.ContentValues.TAG
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,6 +43,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard(){
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
 Surface(modifier = Modifier
     .fillMaxWidth()
     .fillMaxHeight()){
@@ -53,13 +63,64 @@ Surface(modifier = Modifier
             CreateImageProfile()
             Divider()
             CreateProfileInfo()
-        }
+            Button(
+                onClick = {
+                     buttonClickedState.value = !buttonClickedState.value
+                    //Log.d( "Clicked", "CreateBizCard: Clicked") //debug
+                }) {
+                Text(text = "Portfolio",
+                 style = MaterialTheme.typography.button)
 
+                
+            }
+            if (buttonClickedState.value){
+                Content()
+            }else{
+                Box(){
+
+                }
+            }
+
+        }
+        
 
 
 
 
     }
+}
+}
+
+@Composable fun Content(){
+Box(modifier = Modifier
+    .fillMaxHeight()
+    .fillMaxWidth()
+    .padding(5.dp)){
+    Surface(modifier = Modifier
+        .padding(3.dp)
+        .fillMaxWidth()
+        .fillMaxHeight(),
+        shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+       border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+        Portfolio(data = listOf(
+            "Project 1",
+            "Project 2",
+            "Project 3",
+            "Project 4"))
+
+    }
+}
+
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+LazyColumn {
+   items(data){
+       item->
+       Text(item)
+   }
 }
 }
 
@@ -108,8 +169,8 @@ private fun CreateImageProfile(modifier: Modifier= Modifier) {
         )
     }
 }
-
-@Preview(showBackground = true)
+@Preview
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Jetpack_business_cardTheme {
