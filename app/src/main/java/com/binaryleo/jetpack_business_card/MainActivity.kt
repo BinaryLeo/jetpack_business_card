c4931170eb7826d7622ceab340b314db3d0baac7
binaryleo <leomouracss@fastmail.com>
2022-05-28 16:19:32 -0300

    chore: add project list style

--- app/src/main/java/com/binaryleo/jetpack_business_card/MainActivity.kt
+++ app/src/main/java/com/binaryleo/jetpack_business_card/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,8 +22,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.binaryleo.jetpack_business_card.ui.theme.Jetpack_business_cardTheme
@@ -33,8 +37,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Jetpack_business_cardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                CreateBizCard()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CreateBizCard()
                 }
             }
         }
@@ -42,86 +49,119 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreateBizCard(){
+fun CreateBizCard() {
     val buttonClickedState = remember {
         mutableStateOf(false)
     }
-Surface(modifier = Modifier
-    .fillMaxWidth()
-    .fillMaxHeight()){
-    Card(modifier = Modifier
-        .width(200.dp)
-        .height(390.dp)
-        .padding(12.dp),
-        elevation = 4.dp,
-        shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-        backgroundColor = Color.White) {
-        Column(modifier = Modifier.height(300.dp),
-         verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            elevation = 4.dp,
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            backgroundColor = Color.White
+        ) {
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-            CreateImageProfile()
-            Divider()
-            CreateProfileInfo()
-            Button(
-                onClick = {
-                     buttonClickedState.value = !buttonClickedState.value
-                    //Log.d( "Clicked", "CreateBizCard: Clicked") //debug
-                }) {
-                Text(text = "Portfolio",
-                 style = MaterialTheme.typography.button)
+                CreateImageProfile()
+                Divider()
+                CreateProfileInfo()
+                Button(
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                        //Log.d( "Clicked", "CreateBizCard: Clicked") //debug
+                    }) {
+                    Text(
+                        text = "Portfolio",
+                        style = MaterialTheme.typography.button
+                    )
+                }
+                if (buttonClickedState.value) {
+                    Content()
+                } else {
+                    Box {
 
-                
-            }
-            if (buttonClickedState.value){
-                Content()
-            }else{
-                Box(){
-
+                    }
                 }
             }
 
         }
-        
-
-
-
-
     }
 }
-}
 
-@Composable fun Content(){
-Box(modifier = Modifier
-    .fillMaxHeight()
-    .fillMaxWidth()
-    .padding(5.dp)){
-    Surface(modifier = Modifier
-        .padding(3.dp)
-        .fillMaxWidth()
-        .fillMaxHeight(),
-        shape = RoundedCornerShape(corner = CornerSize(6.dp)),
-       border = BorderStroke(width = 2.dp, color = Color.LightGray)
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
         ) {
-        Portfolio(data = listOf(
-            "Project 1",
-            "Project 2",
-            "Project 3",
-            "Project 4"))
+            Portfolio(
+                data = listOf(
+                    "Project 1",
+                    "Project 2",
+                    "Project 3",
+                    "Project 4"
+                )
+            )
 
+        }
     }
-}
 
 }
 
 @Composable
 fun Portfolio(data: List<String>) {
-LazyColumn {
-   items(data){
-       item->
-       Text(item)
-   }
-}
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = 7.dp
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface)
+                ) {
+                    CreateImageProfile(modifier = Modifier.size(size = 100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(
+                            text = "A great project",
+                            style = MaterialTheme.typography.body2
+                        )
+                    }
+
+
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -151,9 +191,9 @@ private fun CreateProfileInfo() {
 }
 
 @Composable
-private fun CreateImageProfile(modifier: Modifier= Modifier) {
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(size = 150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -164,16 +204,17 @@ private fun CreateImageProfile(modifier: Modifier= Modifier) {
         Image(
             painter = painterResource(id = R.drawable.user_profile),
             contentDescription = "user profile picture",
-            modifier = Modifier.size(size = 135.dp),
+            modifier = modifier.size(size = 135.dp),
             contentScale = ContentScale.Crop
         )
     }
 }
+
 @Preview
 //@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Jetpack_business_cardTheme {
-   CreateBizCard()
+        CreateBizCard()
     }
 }
